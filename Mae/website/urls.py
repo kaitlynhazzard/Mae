e81c76ac113306cdfdde6073a161bcb0ab13a13b
Rohan75 <ghotrarohan@gmail.com>
2023-03-06 14:42:52 -0500

    changed template

--- Mae/website/urls.py
+++ Mae/website/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.home),
     path('about/', views.about),
-    path('quote/', views.quote),
 ]
--- Mae/website/views.py
+++ Mae/website/views.py
@@ -2,13 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'website/home.html', context={'home_active': 'active'})
-
+    return render(request, 'website/home.html')
 
 def about(request):
-    return render(request, 'website/about.html', context={'about_active' : 'active'})
-
-
-def quote(request):
-    return render(request, 'website/quote.html', context={'pages_active' : 'active'})
-
+    return render(request, 'website/about.html')
