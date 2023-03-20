25584b4c0335ed14d3c19169b28e0912c6c9ef50
Rohan75 <ghotrarohan@gmail.com>
2023-03-20 17:58:59 -0400

    can now create an account

--- Mae/account/urls.py
+++ Mae/account/urls.py
@@ -6,6 +6,9 @@ from . import views
 app_name = 'account'
 
 urlpatterns = [
+
+    path('register/', views.register, name='register'),
+
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
